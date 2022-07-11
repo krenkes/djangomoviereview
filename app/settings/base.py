39e48d548571f0bcf612646b37be2aa2edf6abdd
Kevin Renkes <kevinrenkes@gmail.com>
2022-07-11 08:20:19 -0500

    feature: Add properties and enquiries

--- app/settings/base.py
+++ app/settings/base.py
@@ -63,6 +63,8 @@ LOCAL_APPS = [
     "apps.common",
     "apps.profiles",
     "apps.ratings",
+    "apps.properties",
+    "apps.enquiries",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
